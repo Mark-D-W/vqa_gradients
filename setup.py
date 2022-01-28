3435e4e3c0b6a95f50f76b12ded6eaf8a3e34196
Mark-D-W <mark.damon.walker@gmail.com>
2022-01-28 12:25:30 +0800

    Add cython dependancy to setup.py

--- setup.py
+++ setup.py
@@ -22,20 +22,20 @@ else:
 
 
 
-VERSION = '0.0.1' 
+VERSION = '0.0.1'
 DESCRIPTION = 'Gradients of variational quantum circuits.'
 #LONG_DESCRIPTION = ''
 
 # Setting up
 setup(
-    name="vqa_gradients", 
+    name="vqa_gradients",
     version=VERSION,
     author="Mark Walker",
     author_email="mark.damon.walker@gmail.com",
     description=DESCRIPTION,
     #long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=["numpy","scipy","matplotlib"],         
+    install_requires=["cython", "numpy","scipy","matplotlib"],
     keywords=['python'],
     classifiers= [
         "Programming Language :: Python :: 3",
